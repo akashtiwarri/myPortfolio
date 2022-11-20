9037ca08c82594c84ab8a6f93ca0375d3cff2a8c
Akash Tiwari <akast2041@gmail.com>
2022-11-19 20:33:40 -0800

    reverting last commit

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./Services.css";
 import services from "../Data/Services.json";
-import webdesignpng from "../assets/img/web-design.png"
-import webdevelopment from "../assets/img/web-development.png"
 
 const Services = () => {
   return (
@@ -11,22 +9,18 @@ const Services = () => {
         <div className="row">
           <div className="col-sm-11 offset-sm-1">
             <h2 className="text-center">What I do</h2>
-            <br />
+            <br/>
           </div>
           <div className="col-sm-11 offset-sm-1">
             <div className="row">
-              <div className="col-sm-6" >
-                <div className="servicebox" data-aos="fade-zoom-in">
-                  <img src={webdesignpng} alt="serviceimg" className="img-fluid" />
-                  <h4>Web Design</h4>
+              {services.map((item, i) => (
+                <div className="col-sm-6" key={i} >
+                  <div className="servicebox" data-aos="fade-zoom-in">
+                    <img src={item.imgg} alt="serviceimg" className="img-fluid" />
+                    <h4>{item.servicename}</h4>
+                  </div>
                 </div>
-              </div>
-              <div className="col-sm-6" >
-                <div className="servicebox" data-aos="fade-zoom-in">
-                  <img src={webdevelopment} alt="serviceimg" className="img-fluid" />
-                  <h4>Web Development</h4>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
